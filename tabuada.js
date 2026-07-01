546ec80c5df2c31ca32d714bcdde633bb57ec30d
Pedro Users <pedroluacas1234@gmail.com>
2026-07-01 20:04:10 -0300

    tabuada.js

---
+++ tabuada.js
@@ -0,0 +1,10 @@
+for (let numero = 1; numero <= 9; numero++) {
+
+    console.log("Tabuada do " + numero);
+
+    for (let i = 1; i <= 9; i++) {
+        console.log(numero + " x " + i + " = " + (numero * i));
+    }
+
+  
+}
